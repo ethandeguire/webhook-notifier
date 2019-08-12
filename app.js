495c045c503fd62824e2ec109ac18f0f920b501c
Ethan <ethandeguire@gmail.com>
2019-08-11 22:01:01 -0400

    sends url to NotifyMe

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const notifier = require('node-notifier')
 const ngrok = require('ngrok')
 const fs = require('fs')
+const axios = require('axios');
 var express = require('express')
 var app = express()
 
 const args = process.argv.slice(2)
-const port = args[0] || "2019"
+args[0] = args[0] == 0 ? 2019 : args[0] 
+const port = args[0] || 2019
+if (port == 'default') port == 2019
+const NotifyUsername = args[1] || null
+const NotifyPassword = args[2] || null
+
 
 const configurations = JSON.parse(fs.readFileSync('webhookproviders.json', 'utf-8'))
 
@@ -54,6 +60,23 @@ app.listen(port, () => {
     console.log(`--ngrok connection succeeded`)
     console.log(`--Webhook POST's sent to the below URL will result in a notification`)
     console.log(`--${url}`)
+
+    // send data to the users account
+    axios.post('https://notifyme.netlify.com/.netlify/functions/update-url-in-db', {
+      data: {
+        username: NotifyUsername,
+        password: NotifyPassword,
+        url: url
+    }
+    })
+      .then((response) => {
+        console.log(`--Url updated in NotifyMe: ${NotifyUsername} - ${url}`);
+      })
+      .catch((error) => {
+        console.log(`** Url failed to update in NotifyMe: ${error.message}: ${error.response.data}`);
+        console.log(`** You can still use the url above to recieve webhooks, but your static url will not be active`);
+      });
+
   })
 });
 
--- setup.bat
+++ setup.bat
@@ -1,1 +1,2 @@
-npm run build
+rem npm run build <port> <NotifyMe Username> <NotifyMe Password>
+npm run build 0 ethandeguire ethanpassword
